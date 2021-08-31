7ab5db1a0ff2802db749ab7f58a10a9628a73b00
volsn <volosnikovkolia@gmail.com>
2021-08-31 10:25:13 +0300

    Modified admin required mixin

--- utils/mixins.py
+++ utils/mixins.py
@@ -1,14 +1,15 @@
 from typing import Dict, Tuple
 
+from flask import request
 from flask_babel import gettext as _
-
-from utils.auth import jwt_required
 
 
 class AdminRequiredMixin:
-    @jwt_required(admin=True)
-    def is_accessible(self) -> bool:
-        return True
+    allowed_hosts = ('127.0.0.1',)
 
-    def inaccessible_callback(self, name: str, **kwargs) -> Tuple[Dict, int]:
+    def is_accessible(self) -> bool:
+        return request.remote_addr in self.allowed_hosts
+
+    @staticmethod
+    def inaccessible_callback(name: str, **kwargs) -> Tuple[Dict, int]:
         return {'message': _('admin_required')}, 400
