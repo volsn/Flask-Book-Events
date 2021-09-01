e1b045f9f00c80a0c87daa6940095d75d3ed7b8c
volsn <volosnikovkolia@gmail.com>
2021-09-01 15:07:13 +0300

    Docstrings and Typing

--- schemas/guest.py
+++ schemas/guest.py
@@ -1,3 +1,8 @@
+"""
+Module with Guest Schema
+"""
+from typing import Dict
+
 from marshmallow import post_load
 
 from ma import ma
@@ -5,9 +10,21 @@ from models.guest import GuestModel
 
 
 class GuestSchema(ma.SQLAlchemyAutoSchema):
+    """
+    Guest Schema
+    """
     class Meta:
+        """
+        Connecting schema to Guest Model
+        """
         model = GuestModel
 
     @post_load
-    def make_guest(self, data, **kwargs):
+    def make_guest(self, data: Dict, **kwargs: Dict) -> GuestModel:
+        """
+        Special method for returning GuestModel object after deserializing
+        :param data: Dict
+        :param kwargs: Dict
+        :return: GuestModel
+        """
         return GuestModel(**data)
--- schemas/participant.py
+++ schemas/participant.py
@@ -1,3 +1,8 @@
+"""
+Module with Participant Schema
+"""
+from typing import Dict
+
 from marshmallow import post_load
 
 from ma import ma
@@ -5,10 +10,23 @@ from models.participant import ParticipantModel
 
 
 class ParticipantSchema(ma.SQLAlchemyAutoSchema):
+    """
+    Participant Schema
+    """
     class Meta:
+        """
+        Connecting schema to Participant Model
+        """
         model = ParticipantModel
         dump_only = ('id',)
 
     @post_load
-    def make_participant(self, data, **kwargs):
+    def make_participant(self, data: Dict, **kwargs: Dict) -> ParticipantModel:
+        """
+        Special method for returning ParticipantModel
+        object after deserializing
+        :param data: Dict
+        :param kwargs: Dict
+        :return: ParticipantModel
+        """
         return ParticipantModel(**data)
